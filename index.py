7e43cc81b48701fd5718663941b5700c5defce96
H.Tajima <tajihiro2@gmail.com>
2016-04-01 18:18:48 +0900

    added ibm_db.

--- index.py
+++ index.py
@@ -3,9 +3,20 @@ import os
 
 app = Flask(__name__)
 
+#環境変数取得
+if 'VCAP_APP_PORT' in os.environ:
+    appPort = os.environ['VCAP_APP_PORT']
+else:
+    appPort = 9000
+
+if 'VCAP_APP_HOST' in os.environ:
+    appHost = os.environ['VCAP_APP_HOST']
+else:
+    appHost = '0.0.0.0'
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
 # サーバ起動
